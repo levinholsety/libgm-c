30922893488508fe70376d9447ec02315e636d2c
Rui Li <levinholsety@hotmail.com>
2020-08-06 09:11:48 +0800

    编写注释。

--- include/sm3.h
+++ include/sm3.h
@@ -7,10 +7,31 @@ typedef unsigned char GM_SM3_MD[32];
 extern "C"
 {
 #endif
+    // 新建并返回一个SM3上下文。
+    // 返回值：返回SM3上下文。失败返回NULL。
     API EVP_MD_CTX *GM_SM3_new();
+
+    // 释放SM3上下文拥有的资源。
     API void GM_SM3_free(EVP_MD_CTX *ctx);
+
+    // 更新SM3上下文中的数据。
+    // 参数1：SM3上下文；
+    // 参数2：需要更新的数据；
+    // 参数3：数据长度。
+    // 返回值：成功返回1，失败返回0，错误返回-1。
     API RESULT GM_SM3_update(EVP_MD_CTX *ctx, const void *in, size_t inlen);
+
+    // 计算并返回SM3哈希值。
+    // 参数1：SM3上下文；
+    // 参数2：用于存储返回的哈希值的变量指针。
+    // 返回值：成功返回1，失败返回0，错误返回-1。
     API RESULT GM_SM3_final(EVP_MD_CTX *ctx, GM_SM3_MD md);
+
+    // 直接计算并返回SM3哈希值。
+    // 参数1：用于存储返回的哈希值的变量指针。
+    // 参数2：需要计算哈希值的数据；
+    // 参数3：数据长度。
+    // 返回值：成功返回1，失败返回0，错误返回-1。
     API RESULT GM_SM3_digest(GM_SM3_MD md, const void *in, size_t inlen);
 #ifdef __cplusplus
 }
